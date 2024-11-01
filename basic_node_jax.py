e0bc75bbdc870021115b18591a539a290309400c
jaisidhsingh <jaisidhsingh@gmail.com>
2024-11-01 12:16:16 +0100

    best attempt with gelu

--- basic_node_jax.py
+++ basic_node_jax.py
@@ -1,5 +1,7 @@
 import jax
 import jax.numpy as jnp
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class MLP:
@@ -11,22 +13,31 @@ class MLP:
             w = jax.random.normal(k, (in_dim, out_dim)) * jnp.sqrt(2.0 / in_dim)
             b = jnp.zeros(out_dim)
             self.params.append((w, b))
+        
+        self.mode = "train"
+
+    def train(self):
+        self.mode = "train"
     
+    def eval(self):
+        self.mode = "eval"
+
     def __call__(self, x):
         for w, b in self.params[:-1]:
             x = jnp.dot(x, w) + b
-            x = jax.nn.tanh(x)
+            x = jax.nn.relu(x)
         w, b = self.params[-1]
-        return jnp.dot(x, w) + b
+        out = jnp.dot(x, w) + b
+        return out[0] if self.mode == "train" else out
 
 
 def actual_trajectory(x, initial_condition):
     """
     Returns the analytical solution of the ODE
-    y = y_0 + 0_/^x x**2 + jnp.sin(x)**2
+    y = y_0 + \int_0^x x**2 + jnp.sin(x)**2
     y = y_0 + 0.5*x + 0.25*jnp.cos(2*x)
     """
-    return initial_condition + (x**3) / 3 + 0.5*x - 0.25*jnp.sin(2*x)
+    return initial_condition + (1/3) * (x**3)
 
 
 def ode_function(x):
@@ -34,42 +45,73 @@ def ode_function(x):
     Returns the slope of the trajectory as defined by the ODE
     dy(x)/dx = f(x, y(x))
     """
-    return x**2 + jnp.sin(x)**2
+    return x**2
 
 
-def loss_function(params, x, model, ic):
+def loss_function(params, x, model, ic, x_0):
     slope_pred = jax.grad(model)(x)
-    slope_loss = (slope_pred - ode_function(x))**2
-    ic_loss = (model(x[0]) - ic)**2
-    loss = (slope_loss + ic_loss)**2
-    return loss
+    slope_loss =  (slope_pred - ode_function(x))**2
+    ic_loss =  (model(x_0) - ic)**2
+    loss = (slope_loss + ic_loss) / 2
+    return loss[0]
 
 
 def update_params(params, grads, lr):
-    new_params = {}
-    for k, v in params.items():
-        new_params[k] = v - lr*grads[k]
-    return new_params
+    new_params = []
+    for (w, b), (grad_w, grad_b) in zip(params, grads[0]):
+        w_new = w - lr*grad_w
+        b_new = b - lr*grad_b
+        new_params.append((w_new, b_new))
+    return tuple(new_params) 
+
+
+def plot_eval(x, y, y_pred):
+    plt.plot(x, y, label="actual")
+    plt.plot(x, y_pred, label="predicted")
+    plt.legend()
+    plt.show()
 
 
 def train_node():
-    num_data_points = 1000
-    x_dim = 10
-    y_dim = 5
-    x = jnp.random.normal(num_data_points, x_dim)
+    upper_bound = 1
+    x_dim = 1
+    y_dim = 1
+    x = jnp.linspace(0, upper_bound)
 
     layer_sizes = [x_dim, 128, 64, y_dim]
     key = jax.random.PRNGKey(0)
     model = MLP(layer_sizes, key)
+    model.train()
 
-    num_epochs = 20
-    initial_condition = jnp.random.normal(10,)
+    num_epochs = 10
+    initial_condition = 0.0 
     lr = 1e-3
 
-    loss_grad = jax.jit(jax.grad(loss_function, argnums=(0,)))
+    loss_grad = jax.grad(loss_function, argnums=(0,))
+    
+    x = x.reshape(x.shape[0], 1)
+    x_0 = x[0]
+    bar = tqdm(total=num_epochs * 50)
     for epoch in range(num_epochs):
-        loss = loss_function(model.params, x, model, initial_condition)
-        grads = loss_grad(model.params, x, model, initial_condition)
-        update_params(model.params, grads, lr)
+        for idx, point in enumerate(x):
+            loss_value = loss_function(model.params, point, model, initial_condition, x_0)
 
+            grads = loss_grad(model.params, point, model, initial_condition, x_0)
+            new_params = update_params(model.params, grads, lr)
+            model.params = new_params
+
+            bar.set_postfix({"Step": epoch * 50 + idx, "loss": loss_value.item()})
+            bar.update(1)
+
+    bar.close()
+
+    model.eval()
+    y_pred = model(x).reshape(50,)
+    x = x.reshape(50,)
+    y = actual_trajectory(x, initial_condition)
+    plot_eval(x, y, y_pred)
+
+
+if __name__ == "__main__":
+    train_node()
 
--- basic_node_torch.py
+++ basic_node_torch.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing_extensions import *
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class NeuralFunctionPredictor(nn.Module):
@@ -21,7 +23,7 @@ def actual_trajectory(x, initial_condition):
     Returns the trajector after solving the ODE analytically
     y = y_0 + integral_{0}^x (x**2 + torch.sin(x)**2 dx)
     """
-    return 0.5 * x - 0.25 * torch.cos(2*x) + initial_condition
+    return (1/3)*x**3 + 0.5 * x - 0.25 * torch.sin(2*x) + initial_condition
 
 
 def ode_function(x):
@@ -29,37 +31,57 @@ def ode_function(x):
     Returns the slope of the trajectory function,
     dy(x)/dx = f(x, y(x))
     """
-    y = torch.sin(x)
-    return x**2 + y**2
+    return x**2 + torch.sin(x)**2
+
+
+def plot_eval(x, y, y_pred):
+    plt.plot(x, y, label="actual")
+    plt.plot(x, y_pred, label="predicted")
+    plt.legend()
+    plt.show()
 
 
 def train_node():
     """
     Here x is a vector and so y is also a vector.
     """
-    x_dim = 10
+    x_dim = 1
     hidden_dim = 5
-    y_dim = 10
+    y_dim = 1
     model = NeuralFunctionPredictor(x_dim, hidden_dim, y_dim)
 
-    dataset = torch.randn(100, x_dim)
+    x = torch.linspace(0, 100, steps=int(1e+4))
     num_epochs = 20
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
+    ic = 0.0
+    y = actual_trajectory(x, ic)
+
+    model.train()
+    bar = tqdm(total=num_epochs)
     for epoch in range(num_epochs):
-        x = dataset
+        x = x.T.unsqueeze(-1)
         optimizer.zero_grad()
         y_pred = model(x)
 
         dy_pred = torch.autograd.grad(y_pred, x, grad_outputs=torch.ones_like(y_pred), create_graph=True)
-        ic_pred = model(x[0])
 
-        slope_residual = (ode_function(x) - y_pred)**2
-        initial_condition_residual = (actual_trajectory(x[0]) - ic_pred)**2
+        slope_residual = (ode_function(x) - dy_pred)**2
+        initial_condition_residual = (ic - y_pred[0])**2
         loss = slope_residual + initial_condition_residual
+
+        running_loss = loss.item()
 
         loss.backward()
         optimizer.step()
+        
+        bar.set_postfix({"epoch": epoch+1, "loss": running_loss})
+        bar.update(1)
+
+    model.eval()
+    y_pred = model(x)
+    plot_eval(x, y, y_pred)
 
 
-
+if __name__ == "__main__":
+    train_node()
